88a24ce9ff291df25eb10ee5c86689b5004e9097
roughk <46721827+roughk@users.noreply.github.com>
2019-10-04 16:32:49 -0400

    Establish Android App Files
    Populated Android App files

---
+++ AndroidApp/AppFiles/app/src/main/java/rcos/ai_gym_tracker/MainActivity.java
@@ -0,0 +1,13 @@
+package rcos.ai_gym_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
