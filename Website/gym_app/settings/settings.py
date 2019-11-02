29b02516fce26ee48a3ce4f986abcbbc1e06081e
Gabe <gabeorlanskI@gmail.com>
2019-11-02 18:17:38 -0400

    Added in lines to import rest frame work
    also added it to requirements.txt

--- Website/gym_app/settings/settings.py
+++ Website/gym_app/settings/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authentication'
 ]
 
 MIDDLEWARE = [
