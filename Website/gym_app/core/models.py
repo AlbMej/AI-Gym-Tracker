21ee4a0b0cf7a64160443606a029cd222d69c2d9
James <jamesweir11@gmail.com>
2019-11-15 16:21:27 -0500

    added a user model

--- Website/gym_app/core/models.py
+++ Website/gym_app/core/models.py
@@ -34,3 +34,8 @@ class Exercise(models.Model):
     exID = models.IntegerField()
     name = models.CharField(max_length=64)
     primary = models.CharField(max_length=32)
+
+class User(models.Model):
+    uID = models.IntegerField()
+    username = models.CharField(max_length=32)
+    email = models.CharField(max_length=64)
