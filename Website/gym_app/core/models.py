eb64a42f6231b3c383c4243bf34c4faf5bc1e59e
James <jamesweir11@gmail.com>
2019-12-03 16:51:45 -0500

    created new models representing schema

--- Website/gym_app/core/models.py
+++ Website/gym_app/core/models.py
@@ -32,11 +32,51 @@ class Loan(models.Model):
 #A test model, going to experiment to see how we can add these to a database
 class Exercise(models.Model):
     exID = models.IntegerField()
-    name = models.CharField(max_length=64)
-    primary = models.CharField(max_length=64)
-    secondary = models.CharField(max_length=256)
+    name = models.CharField(max_length=64)          #Exercise name
+    primary = models.CharField(max_length=64)       #Primary muscle group
+    secondary = models.CharField(max_length=256)    #Secondary muscle groups
+
+class RoutineExercise(models.Model):
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    sets = models.IntegerField()
+    reps = models.IntegerField()
+
+class Routine(models.Model):
+    routineName = models.CharField(max_length=64)
+    exercises = models.ArrayModelField(
+        model_container=RoutineExercise,
+    )
+
+class LogDay(models.Model):
+    day = models.IntegerField()             #Day of the month (1-31 typically)
+    month = models.IntegerField()           #Month represented by integer 1-12
+    year = models.IntegerField()            #Year (i.e. 2019)
+    routines = ArrayField(
+        models.CharField(max_length=64),
+    )
+
+class LogMonth(models.Model):
+    month = models.IntegerField()           #Month represented by integer 1-12
+    year = models.IntegerField()            #Year (i.e. 2019)
+    days = models.ArrayModelField(
+        model_container=LogDay
+    )
+
+class LogYear(models.Model):
+    year = models.IntegerField()            #Year (i.e. 2019)
+    months = models.ArrayModelField(
+        model_container=LogMonth,
+    )
+
+class Log(models.Model):
+    years = models.ArrayModelField(
+        model_container=LogYear,
+    )
 
 class User(models.Model):
     uID = models.IntegerField()
     username = models.CharField(max_length=32)
     email = models.CharField(max_length=64)
+    routines = models.ArrayModelField(
+        model_container=Routine,
+    )
