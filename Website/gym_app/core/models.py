b7ef8c0ded6f616e38fd381c3ecb387977e632d7
WillieXia <WillieXia@users.noreply.github.com>
2020-04-28 16:22:37 -0400

    New Front End

--- Website/gym_app/core/models.py
+++ Website/gym_app/core/models.py
@@ -2,33 +2,6 @@ from djongo import models
 
 # Create your models here.
 
-class Loan(models.Model):
-    BUSINESS_TYPES = (
-    ('', 'Choose...'),
-    ('FT', 'Food Truck'),
-    ('CON', 'Construction'),
-    ('OTH', 'Other')
-    )
-
-    first_name = models.CharField(max_length=64)
-    last_name = models.CharField(max_length=64)
-    email = models.EmailField()
-
-    phone = models.CharField(max_length=32)
-
-    address = models.CharField(max_length=256)
-
-    city = models.CharField(max_length=64)
-    state = models.CharField(max_length=32)
-    zip_code = models.CharField(max_length=32)
-
-    amount_required = models.IntegerField() #label = 'Amount required'
-    business_type = models.CharField(max_length=3, choices=BUSINESS_TYPES)
-    years_in_business = models.IntegerField() #label='Years in business'
-    other = models.CharField(max_length=64)
-
-    agree = models.BooleanField()  #required=True
-
 #A test model, going to experiment to see how we can add these to a database
 class Exercise(models.Model):
     exID = models.IntegerField()
